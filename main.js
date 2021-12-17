cfbd9c34aefeccd7a917877224a4fd5ddaaaa512
Rafael Ramirez <susurrus.ater@gmail.com>
2021-12-17 19:56:29 -0300

    add details

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             description: 'awesome socks',
             image: './assets/images/socks_green.jpg',
-            inventory: 100
+            inventory: 100,
+            details: ['50% cotton', '50% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ]
         }
     }
 })
